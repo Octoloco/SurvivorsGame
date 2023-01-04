661bb0436178814aa5996b96d4404376b247a56d
Diego Clorio <diego_clorio@hotmail.com>
2023-01-03 20:33:21 -0600

    Finished Factory and basic enemy spawner

--- Survivors Game AI/Assets/Scripts/Misc/Spawner.cs
+++ Survivors Game AI/Assets/Scripts/Misc/Spawner.cs
@@ -6,8 +6,10 @@ public class Spawner : MonoBehaviour
 {
     private Vector2 dimensions0;
     private Vector2 dimensions1;
+    private float timer = 0;
 
-    private Factory EnemyFactory;
+    [SerializeField] private Factory EnemyFactory;
+    [SerializeField] private float startTime;
 
     void Start()
     {
@@ -17,16 +19,53 @@ public class Spawner : MonoBehaviour
 
     void Update()
     {
-        
+        if (timer <= 0)
+        {
+            SpawnEnemy();
+            timer = startTime;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
     }
 
     private void SpawnEnemy()
     {
         GameObject temp = EnemyFactory.CreateItem();
         Vector2 position;
-        position.x = Random.Range(dimensions0.x, dimensions1.x);
-        position.y = Random.Range(dimensions0.y, dimensions1.y);
+        if (Random.Range((int)0, (int)2) == 0)
+        {
+            position.x = Random.Range(dimensions0.x, dimensions1.x);
+            if (Random.Range((int)0, (int)2) == 0)
+            {
+                position.y = dimensions0.y;
+            }
+            else
+            {
+                position.y = dimensions1.y;
+            }
+        }
+        else
+        {
+            if (Random.Range((int)0, (int)2) == 0)
+            {
+                position.x = dimensions0.x;
+            }
+            else
+            {
+                position.x = dimensions1.x;
+            }
+            position.y = Random.Range(dimensions0.y, dimensions1.y);
+        }
 
         temp.transform.position = position;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
     }
 }
