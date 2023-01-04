4e7200303d041ecbe09a3f523318660c4e3305dd
Diego Clorio <diego_clorio@hotmail.com>
2023-01-03 21:03:49 -0600

    Finished basic actor

--- Survivors Game AI/Assets/Scripts/Enemies/Actor.cs
+++ Survivors Game AI/Assets/Scripts/Enemies/Actor.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Actor : MonoBehaviour
 {
+    [SerializeField] private StatsSO statSheet; 
+
+    private int currentHealth;
     private float testTimer = 3;
+
     void Start()
     {
-        
+
     }
 
     void Update()
@@ -15,13 +19,35 @@ public class Actor : MonoBehaviour
 
     }
 
-    public void ResetActor()
+    public void subtractHealth(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            DestroyActor();
+        }
+    }
+
+    public void addHealth(int heal)
+    {
+        if (currentHealth + heal > statSheet.health)
+        {
+            currentHealth = statSheet.health;
+        }
+        else
+        {
+            currentHealth += heal;
+        }
+    }
+
+    public virtual void ResetActor()
     {
 
     }
 
     public void DestroyActor()
     {
-
+        transform.parent.GetComponent<Factory>().DestroyItem(gameObject);
     }
 }
